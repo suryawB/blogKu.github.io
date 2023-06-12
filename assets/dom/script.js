0625c2fdffed92a6b4839728d917c67fc0d1f875
suryawB <78914406+suryawB@users.noreply.github.com>
2023-06-12 17:51:18 +0700

    Add files via upload

---
+++ assets/dom/script.js
@@ -0,0 +1,8 @@
+//Dom JavaScript 
+//ubah tema background.
+
+const ubahTema = document.getElementById('ubahTema');
+
+ubahTema.onclick = function(){
+  document.body.classList.toggle('biru-muda');
+}
